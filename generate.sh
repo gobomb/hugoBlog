06d5d046001c8f5e8e0a4b9d212a192cba45a7e1
gobomb <bombcoder@outlook.com>
2020-10-12 02:00:54 +0800

    rm even and backup header.html

--- generate.sh
+++ generate.sh
@@ -6,3 +6,5 @@ cd  $BLOGPATH && hugo --theme=even --baseUrl="https://gobomb.github.io/"
 # update submodule
 git diff --cached --submodule
 
+# add GA
+cp header.html.bak themes/basics/layouts/partials/header.html
