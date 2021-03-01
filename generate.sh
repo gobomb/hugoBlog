495fb5c43b7990fb62e54104fb566c5e7a77b3a3
gobomb <bombcoder@outlook.com>
2021-03-02 02:57:36 +0800

    save hugo version

--- generate.sh
+++ generate.sh
@@ -1,10 +1,14 @@
 BLOGPATH=`pwd`
 
 
-cd  $BLOGPATH && hugon --theme=basics --baseUrl="https://gobomb.github.io/"
+cd  $BLOGPATH && hugo --theme=basics --baseUrl="https://gobomb.github.io/"
 
 ## update submodule
 # git diff --cached --submodule
 
 ## add GA
 # cp bak/header.html layouts/partials/header.html
+
+## save version of hugo
+# echo "#" `hugo version` >> generate.sh
+# hugo v0.81.0-59D15C97 darwin/amd64 BuildDate=2021-02-19T17:07:12Z VendorInfo=gohugoio
